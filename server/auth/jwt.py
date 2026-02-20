4df15c211f9fe17e07fbb069925d8185df53c2d2
john.barsigo@student.moringaschool.com <john.barsigo@student.moringaschool.com>
2026-02-20 16:47:04 +0300

    Added token generation and decoding in jwt.py.

---
+++ server/auth/jwt.py
@@ -0,0 +1,43 @@
+
+import jwt
+from datetime import datetime, timedelta
+from flask import current_app
+from models import User
+
+DEFAULT_EXPIRATION_DELTA = timedelta ( hours = 1 )
+DEFAULT_SECRET_KEY = "e7ba32d2feaa467398beb846112494c5"
+
+
+def generate_token ( user_id, role, expiration_delta = DEFAULT_EXPIRATION_DELTA, secret_key = DEFAULT_SECRET_KEY ) :
+
+    payload = {
+        "id" : user_id,
+        "role" : role,
+        "exp" : datetime.utcnow ( ) + expiration_delta
+    }
+
+    token = jwt.encode (
+        payload,
+        secret_key,
+        algorithm = "HS256"
+    )
+
+    return token
+
+
+def decode_token ( token, secret_key = DEFAULT_SECRET_KEY ) :
+
+    try :
+        payload = jwt.decode (
+            token,
+            secret_key,
+            algorithms = [ "HS256" ]
+        )
+    
+    except jwt.ExpiredSignatureError :
+        return None, "Token has expired."
+    
+    except jwt.InvalidTokenError :
+        return None, "Invalid token."
+    
+    return payload [ "id" ], None
