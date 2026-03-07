8271e401c99635dc05bd5fb55fe665574cf190f5
john.barsigo@student.moringaschool.com <john.barsigo@student.moringaschool.com>
2026-03-07 23:26:08 +0300

    Some code cleanup

--- server/routes/payments.py
+++ server/routes/payments.py
@@ -87,8 +87,3 @@ class PaymentDetails ( Resource ) :
             "payment_date" : str (payment.payment_date.isoformat()),
             "created_at" : str (payment.created_at.isoformat())
         }
-
-
-
-
-# ================ CREATE OCCUPANCY USING TENANTS.PY AND TEST DELETE OCCUPANCY USING OCCUPANCIES.PY ================
